40b400522bc74067d5f6cc31ba0e3488314cd40f
Nandini MudhiReddy <82887305+nandinireddy3@users.noreply.github.com>
2023-11-29 03:52:43 +0530

    Create CycleDetection_UndirectedGraph[DFS].cpp
    added

---
+++ CycleDetection_UndirectedGraph[DFS].cpp
@@ -0,0 +1,39 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cycleDetect(int node, int parent, vector<int> adj[], vector<bool>& vis){
+    
+    vis[node] = true;
+    for(auto u: adj[node]){
+        if(!vis[u]){
+            if(dfs(u,v,adj,vis)) return true;
+        }
+        else if(u != parent){
+            return true;
+        }
+    }
+    
+    return false;
+}
+int main() {
+    // Write C++ code here
+    int n = 6;
+    vector<vector<int>> edges={{0,3},{1,2},{1,5},{2,4},{3,5},{5,4},{5,0}};
+
+    vector<int> adj[n];
+
+    for(int i = 0; i < edges.size(); i++){
+        adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]);
+    }
+    vector<bool> vis[n];
+    for(int i = 0; i < n; i++){
+        if(vis[i] == false){
+            if(cycleDetect(i,-1,adj,vis)) // cycle detected
+        }
+    }
+    // no cycle detected
+    return 0;
+}
